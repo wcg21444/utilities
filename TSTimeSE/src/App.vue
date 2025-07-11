3d1cc3df87df37c48a06df9334c454798525aa43
wcg21444 <1789810694@qq.com>
2025-07-11 10:54:45 +0800

    迁移TimeSE到TS

--- TSTimeSE/src/App.vue
+++ TSTimeSE/src/App.vue
@@ -1,27 +1,30 @@
-<script setup>
+<script setup lang="ts">
 import { ref, computed } from 'vue'
 import dayjs from 'dayjs'
+import type { Dayjs } from 'dayjs'
 
-const backward = ref('0');
-const zeor_time = new Date("2025/1/1 00:00:00");
-const time_start = ref();
-const time_end = ref();
-const time_point = ref();
-const time_diff = ref();
+interface UnitOption {
+  value: 'sec' | 'min' | 'hour'
+  label: string
+}
+
+const backward = ref<'0' | '1'>('0');
+const zero_time = new Date("2025/1/1 00:00:00");
+const time_start = ref<Dayjs>();
+const time_end = ref<Dayjs>();
+const time_point = ref<Dayjs>();
+const time_diff = ref<Dayjs>();
 const time_interval = computed(() => {
   if (!time_end.value || !time_start.value) {
     return ``;
   }
   else {
-    let sec = Math.floor((time_end.value - time_start.value) / 1000);
+    let sec = Math.floor((time_end.value.valueOf() - time_start.value.valueOf()) / 1000);
     if (unit.value == 'sec') { return `${sec} s` }
     if (unit.value == 'min') {
       let min = Math.floor(sec / 60);
       sec -= min * 60;
       return `${min} min,${sec} s`;
-
-      //Why NaN?  Because You Returned diff.val , which is not the real diff ms
-      //Bad name diff
     }
     if (unit.value == 'hour') {
       let hour = Math.floor(sec / 3600);
@@ -30,6 +33,7 @@ const time_interval = computed(() => {
       sec -= min * 60;
       return `${hour} hour, ${min} min,${sec} s`;
     }
+    return '';
   };
 });
 
@@ -44,23 +48,24 @@ const new_time_point = computed(() => {
     const minutes = diffTime.minute();
     const seconds = diffTime.second();
 
-    if (backward.value == 0) {
+    if (backward.value == '0') {
       return baseTime
         .add(hours, 'hour')
         .add(minutes, 'minute')
         .add(seconds, 'second')
-        .format('HH:mm:ss');
+        .format('HH:mm');
     }
     else {
       return baseTime
         .subtract(hours, 'hour')
         .subtract(minutes, 'minute')
         .subtract(seconds, 'second')
-        .format('HH:mm:ss');
+        .format('HH:mm');
     }
   }
 });
-const unit_options = [
+
+const unit_options: UnitOption[] = [
   {
     value: 'sec',
     label: 'sec',
@@ -75,9 +80,11 @@ const unit_options = [
   },
 ]
 
-const unit = ref('min');
+const unit = ref<'sec' | 'min' | 'hour'>('min');
 </script>
+
 <template>
+  <p>时间间隔计算</p>
   <div>
     <div class="time-picker">
       <el-time-picker v-model="time_start" placeholder="Start Time" />
@@ -87,26 +94,58 @@ const unit = ref('min');
       <el-select v-model="unit" placeholder="Unit" size="large" style="width: 90px">
         <el-option v-for="item in unit_options" :key="item.value" :label="item.label" :value="item.value" />
       </el-select>
-      <a>Time Interval:{{ time_interval.valueOf() }}</a>
+      <p class="time-point">Time Interval:{{ time_interval }}</p>
     </div>
   </div>
 
   <div>
-    <el-time-picker v-model="time_point" placeholder="Time Point" />
-    <el-time-picker v-model="time_diff" placeholder="Arbitrary time" :default-value="zeor_time" />
-    <div class="mb-2 ml-4">
+    <p>时间推算</p>
+    <el-time-picker v-model="time_point" placeholder="Time Point" format="H:mm" />
+    <el-time-picker v-model="time_diff" placeholder="Arbitrary time" :default-value="zero_time" format="H:mm" />
+    <div class="select-button">
       <el-radio-group v-model="backward">
         <el-radio-button value="0" size="large">向后</el-radio-button>
         <el-radio-button value="1" size="large">向前</el-radio-button>
       </el-radio-group>
     </div>
-    <a>{{ new_time_point }}</a>
+    <p class="time-point">新的时间点:{{ new_time_point }}</p>
   </div>
 </template>
+<!-- TODO : Time Allocator  -->
+<!--  
+- 0:00      <--begin
+- 1:00          description
+- 2:00          3hours
+  - 2:15
+  - 2:20
+  - 2:25    <-- sub-begin
+  - 2:30
+  - 2:35    <-- sub-end
+  - 2:40  
+  - 2:45    <-- sub-begin
+  - 2:50    <-- sub-end
+  - 2:55
+- 3:00      <--end
+- 4:00
+- 5:00
+- 6:00      <--begin     descrpition
+- 7:00      <--end       1hour
+- 8:00
+- 9:00
+- 10:00
+...
 
-<!-- TODO: 优化界面布局 -->
+如何实现时间分配器?
+  如何添加时间刻度组件?
+ -->
 <style scoped>
 .time-picker {
   margin: 8px;
+}
+
+.time-point {
+  margin: 8px;
+  font-size: 16px;
+  color: #409eff;
 }
 </style>
