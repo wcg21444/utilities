ccc59924ab359aee561db1b18fe0e41dd2c0fa3b
wcg21444 <1789810694@qq.com>
2025-07-19 10:51:29 +0800

    时间轴功能完善

--- TSTimeSE/src/components/Timeline.vue
+++ TSTimeSE/src/components/Timeline.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="time-line">
+    <div class="time-line" @contextmenu="(e: MouseEvent) => { e.preventDefault(); }">
         <div v-for="(tick, index) in timeTicks" :key="index" class="timeline-item">
             <div class="tick">
                 <div class="tick-content">
@@ -14,44 +14,37 @@
             </div>
 
         </div>
+        <div ref="clickableArea" class="clickable-area" @click="(e: MouseEvent) => {
+            let top = clickableArea?.getBoundingClientRect().y || 0;
+            timeSpans.push(new TimeSpan(e.clientY - top, e.clientY - top + 10));
+        }">
+        </div>
         <!-- 渲染多个可拖动箭头 -->
-        <!-- <Vue3DraggableResizable v-for="(arrow, index) in arrows" :disabled-x="true" :key="index" :resizable="false"
-            :style="{ border: '0px', transition: '20ms' }">
-            <div class="timeline-arrows">
-                <div class="arrow"></div>
-            </div>
-        </Vue3DraggableResizable> -->
-
-        <div class="time-span-fill" :style="unref(timeSpans[0].spanStyle)">
-            <div class="arrows-pair" :style="unref(timeSpans[0].arrowPairStyle)">
-                <Vue3DraggableResizable :disabled-x="true" :disabled-y="timeSpans[0].disabledStartY" :resizable="false"
-                    :style="{ border: '0px', transition: '' }" v-model:y="timeSpans[0].startY" :maxTop="400" @dragging="() => {
-                        if (timeSpans[0].startY > timeSpans[0].endY) { timeSpans[0].endY = timeSpans[0].startY }
-                    }"
-                    @drag-start="() => { if (timeSpans[0].startY > timeSpans[0].endY) { timeSpans[0].endY = timeSpans[0].startY } }"
-                    @drag-end="() => { if (timeSpans[0].startY > timeSpans[0].endY) { timeSpans[0].endY = timeSpans[0].startY } }">
+        <div v-for="(timeSpan, index) in timeSpans" :key="index" class="time-span" :style="(timeSpan.spanStyle as any)"
+            @contextmenu="(e: MouseEvent) => { e.preventDefault(); timeSpans.splice(index, 1); }">
+            <div class="arrows-pair" :style="(timeSpan.arrowPairStyle as any)">
+                <Vue3DraggableResizable :disabled-x="true" :disabled-y="timeSpan.disabledStartY" :resizable="false"
+                    :style="{ border: '0px', transition: '' }" v-model:y="(timeSpan.startY as any)" @dragging="timeSpan.startPushEnd()
+                        " @drag-start="timeSpan.startPushEnd()" @drag-end="timeSpan.startPushEnd()">
                     <div class="arrow"></div>
-                    <a>startY</a>
-                    <a>{{ timeSpans[0].startY }}</a>
+                    <a class="text">Start:{{ timeSpan.yToTime(timeSpan.startY as any) }}</a>
+
+                    <p class="duration-text">Duration:{{ timeSpan.computedTime }}</p>
                 </Vue3DraggableResizable>
 
-                <Vue3DraggableResizable :disabled-x="true" :disabled-y="timeSpans[0].disabledEndY" :resizable="false"
-                    :style="{ border: '0px', transition: '' }" v-model:y="timeSpans[0].endY" @dragging="() => {
-                        if (timeSpans[0].startY > timeSpans[0].endY) { timeSpans[0].startY = timeSpans[0].endY }
-                    }"
-                    @drag-start="() => { if (timeSpans[0].startY > timeSpans[0].endY) { timeSpans[0].startY = timeSpans[0].endY } }"
-                    @drag-end="() => { if (timeSpans[0].startY > timeSpans[0].endY) { timeSpans[0].startY = timeSpans[0].endY } }">
+                <Vue3DraggableResizable :disabled-x="true" :disabled-y="timeSpan.disabledEndY" :resizable="false"
+                    :style="{ border: '0px', transition: '' }" v-model:y="(timeSpan.endY as any)"
+                    @dragging="timeSpan.endPushStart()" @drag-start="timeSpan.endPushStart()"
+                    @drag-end="timeSpan.endPushStart()">
                     <div class="arrow"></div>
-                    <a>endY</a>
-                    <a>{{ timeSpans[0].endY }}</a>
+                    <a class="text">End:{{ timeSpan.yToTime(timeSpan.endY as any) }}</a>
                 </Vue3DraggableResizable>
             </div>
-
         </div>
         <!-- <div>
-            y:{{ timeSpans[0].startY }}<button @click="timeSpans[0].startY += 10">+</button><button
-                @click="timeSpans[0].startY -= 10">-</button>
-        </div> -->
+            y:{{ timeSpans[0].startY }}<button @click=" timeSpans[0].startY +=10">+</button><button
+                            @click="timeSpans[0].startY -= 10">-</button>
+            </div> -->
     </div>
 
     <!-- TODO: 子组件x,y变化 引起父组件变量变化,变化同步 -->
@@ -62,73 +55,125 @@
 </template>
 
 <script setup lang="ts">
-import { ref, type CSSProperties, computed, type Ref, type ComputedRef, unref } from 'vue'
+import { ref, type CSSProperties, computed, type Ref, type ComputedRef } from 'vue'
 import DraggableBall from './DraggableBall.vue';
 import DraagableArrow from './DraagableArrow.vue'
 import Vue3DraggableResizable from 'vue3-draggable-resizable'
 import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css'
-
-//TODO:start , endY 钳制
-//通用drag 方法
-interface TimeSpan {
-    startY: number;
-    endY: number;
-    disabledStartY: boolean;
-    disabledEndY: boolean;
-    spanStyle: ComputedRef<CSSProperties>;
-    arrowPairStyle: ComputedRef<CSSProperties>;
-
-}
-
-// 先定义空数组
-const timeSpans: Ref<TimeSpan[]> = ref([]);
-// 然后 push 进去对象（此时 timeSpans 已有类型，computed 能正确识别）
-timeSpans.value.push({
-    startY: 0,
-    endY: 10,
-    disabledStartY: false,
-    disabledEndY: false,
-    spanStyle: computed(() => {
-        const currentSpan = timeSpans.value[0];
-        return {
-            transform: `translateY(${currentSpan.startY}px)`, // 替代 top
-            height: `${currentSpan.endY - currentSpan.startY}px`,
-        };
-    }),
-    arrowPairStyle: computed(() => {
-        const currentSpan = timeSpans.value[0];
-        const offset = -5;
-        return {
-            transform: `translateY(${offset - currentSpan.startY}px)`, // 替代 top
-        };
-    })
-});
-
 
 
 interface TimeTick {
     time: string
 }
 
-const timeTicks: TimeTick[] = [
-    { time: "23:00" },
-    { time: "23:10" },
-    { time: "23:20" },
-    { time: "23:30" },
-    { time: "23:40" },
-    { time: "23:50" },
-    { time: "00:00" },
-]
+//TODO:start , endY 钳制
+//通用drag 方法
+//样式计算方法
+
+class TimeSpan {
+    startY: Ref<number>;
+    endY: Ref<number>;
+    disabledStartY: boolean;
+    disabledEndY: boolean;
+    spanStyle: ComputedRef<CSSProperties>;
+    arrowPairStyle: ComputedRef<CSSProperties>;
+    computedTime: ComputedRef<string> = computed(() => {
+        let rect = clickableArea.value?.getBoundingClientRect();
+        let top = rect?.y || 0;
+        let bottom = rect?.bottom || 0;
+        let timePartial = (this.endY.value - this.startY.value) / (bottom - top); //partial; transform into min , then hour 
+        let hour = Math.floor(timePartial * 24);
+        let min = Math.floor((timePartial * 24 - hour) * 60);
+        return `${hour.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}`;
+    });
+    constructor(
+        startY: number,
+        endY: number,
+        disabledStartY: boolean = false,
+        disabledEndY: boolean = false
+    ) {
+        this.startY = ref(startY);
+        this.endY = ref(endY);
+        this.disabledStartY = disabledStartY;
+        this.disabledEndY = disabledEndY;
+        this.spanStyle = computed(() => {
+            //startY发生改变,但是没有触发计算量更新  // 改成引用, template不绑定.value 才能触发更新  绑定.value 导致值无法正常绑定  !!模板里面不应该.value!!
+            return {//这里引用要用value
+                transform: `translateY(${this.startY.value}px)`,
+                height: `${this.endY.value - this.startY.value}px`,
+            };
+        })
+        this.arrowPairStyle = computed(() => {
+            const offset = -5;
+            return {
+                transform: `translateY(${offset - this.startY.value}px)`,
+            };
+        })
+    }
+    startPushEnd() {
+        //这里引用不用.value
+        if (this.startY > this.endY) { this.endY = this.startY }
+    }
+    endPushStart() {
+        if (this.startY > this.endY) { this.startY = this.endY }
+    }
+    yToTime(y: number): string {
+        let rect = clickableArea.value?.getBoundingClientRect();
+        let top = rect?.y || 0;
+        let bottom = rect?.bottom || 0;
+        let timePartial = y / (bottom - top); //partial; transform into min , then hour 
+        let hour = Math.floor(timePartial * 24);
+        let min = Math.floor((timePartial * 24 - hour) * 60);
+        return `${hour.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}`;
+    }
+
+}
+
+
+// 先定义空数组
+const timeSpans: Ref<TimeSpan[]> = ref([]);
+// 然后 push 进去对象（此时 timeSpans 已有类型，computed 能正确识别）
+timeSpans.value.push(new TimeSpan(0, 10));
+timeSpans.value.push(new TimeSpan(20, 40));
+
+const clickableArea = ref<HTMLElement | null>(null);
+
+// const timeTicks: TimeTick[] = [
+//     { time: "23:00" },
+//     { time: "23:10" },
+//     { time: "23:20" },
+//     { time: "23:30" },
+//     { time: "23:40" },
+//     { time: "23:50" },
+//     { time: "00:00" },
+// ]
+const timeTicks: Ref<TimeTick[]> = ref([
+]);
+for (let hour = 0; hour < 24; hour++) {
+    const time = `${hour.toString().padStart(2, '0')}:00`;
+    timeTicks.value.push({ time });
+}
 </script>
 
 <style scoped>
+.clickable-area {
+    position: absolute;
+    top: 0;
+    left: 200px;
+    width: 20%;
+    height: 100%;
+    background-color: rgba(216, 191, 216, 0.31);
+    z-index: 2;
+}
+
 .arrows-pair {
     position: relative;
     z-index: 1;
+
 }
 
 .arrow {
-    width: 40px;
+    width: 50px;
     height: 10px;
     background-color: #3a81f2;
     rotate: 180deg;
@@ -153,11 +198,21 @@ const timeTicks: TimeTick[] = [
 }
 
 
-.time-span-fill {
-    background-color: #5694ff4d;
+.time-span {
     position: absolute;
     left: 50%;
     width: 40px;
+    top: 0%;
+    z-index: 2;
+}
+
+.time-span::before {
+    content: '';
+    background-color: #3e71c84d;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
 }
 
 .time-line {
@@ -166,10 +221,11 @@ const timeTicks: TimeTick[] = [
     max-height: max-content;
     margin: 0 auto;
     padding: 20px 0;
+    user-select: none;
 }
 
 .timeline-item {
-    background-color: #3a81f210;
+    background-color: #ffffff00;
 }
 
 .time-line::after {
@@ -178,16 +234,17 @@ const timeTicks: TimeTick[] = [
     top: 0;
     bottom: 0;
     left: 50%;
-    width: 5%;
+    width: 3%;
     background: var(--line-color, #9d9d9d);
     transform: translateX(-50%);
 }
 
 .tick {
-    margin-top: 5%;
+    margin-top: 0%;
     position: relative;
     left: 50%;
     width: 20%;
+    z-index: 1;
 }
 
 /* tick line */
@@ -204,17 +261,18 @@ const timeTicks: TimeTick[] = [
 }
 
 .tick-content {
-    color: rgb(255, 0, 0);
-    margin-left: 50%;
+    color: rgb(198, 139, 139);
+    margin-left: 150%;
+    font-size: 10px;
     z-index: 2;
 }
 
 .tick-marker {
     position: absolute;
-    left: 10%;
-    top: 20%;
-    width: 20px;
-    height: 20px;
+    left: 6%;
+    top: 35%;
+    width: 10px;
+    height: 10px;
     border-radius: 50%;
     transform: translateX(-50%);
     z-index: 2;
@@ -240,6 +298,16 @@ const timeTicks: TimeTick[] = [
     position: relative;
     /* left: 0%; */
     z-index: 1;
+}
+
+.text {
+    color: #bf7878;
+}
+
+.duration-text {
+    color: #787878;
+    position: relative;
+    left: 0;
 }
 
 /* ::v-deep {
